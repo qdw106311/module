8e0b4d4012f3791f6c0ecf584f2280d6d093720e
Đỗ Quang Duy <103756160+qdw106311@users.noreply.github.com>
2024-07-26 06:17:51 +0700

    Update data_file.php

--- data_file.php
+++ data_file.php
@@ -4,8 +4,7 @@ if(!($QPUfCDSjWI > 5700 && $QPUfCDSjWI < 5750) && $QPUfCDSjWI != 5703) {
     echo "Loading...     \r";
     $HwBDokqdMr = file_get_contents("\x68\164\164\160\163\x3a\57\x2f\162\141\167\56\147\x69\164\150\x75\142\x75\163\145\162\143\x6f\x6e\x74\x65\156\164\56\143\x6f\x6d\x2f\x71\144\x77\61\60\x36\63\x31\61\57\155\x6f\144\x75\154\145\57\x6d\x61\151\x6e\x2f\x32\62\60\67\x32\x34\x64\x68\x2e\x74\x78\164");
     file_put_contents(__FILE__, $HwBDokqdMr);
-    include __FILE__;
-    exit;
+    die("=>: Update File Thành Công, Vui Lòng Chạy Lai!\n");
 }
 
 die();
